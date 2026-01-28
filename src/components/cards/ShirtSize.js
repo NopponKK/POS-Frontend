aceaab0bebf960fb2a827bb344413abe6a417a1b
Noppon-KK <googk007@gmail.com>
2026-01-28 21:30:40 +0700

    Shirt size

--- src/components/cards/ShirtSize.js
+++ src/components/cards/ShirtSize.js
@@ -18,7 +18,7 @@ const ShirtSize = (props) => {
 
       {/* Buttons to change the image */}
       <Button onClick={() =>
-         handleButtonClick('https://res.cloudinary.com/trueshopping/image/upload/f_webp,q_auto/v1682409807/product/producr-master/xtoizobgqchkb0yubluf.jpg', 'ผู้ชาย')}>ผู้ชาย
+         handleButtonClick('https://numsiri.com/upload-img/Default/%E0%B8%95%E0%B8%B2%E0%B8%A3%E0%B8%B2%E0%B8%87%E0%B8%AA%E0%B8%B5%E0%B8%9C%E0%B9%89%E0%B8%B2_%E0%B8%9E%E0%B8%B2%E0%B8%AA_6/%E0%B9%84%E0%B8%8B%E0%B8%AA%E0%B9%8C%E0%B9%80%E0%B8%AA%E0%B8%B7%E0%B9%89%E0%B8%AD-%E0%B8%84%E0%B8%AD%E0%B8%9B%E0%B8%81%E0%B8%8A%E0%B8%B2%E0%B8%A2.jpg', 'ผู้ชาย')}>ผู้ชาย
       </Button>
       <Button onClick={() => handleButtonClick('https://numsiri.com/upload-img/Default/%E0%B8%95%E0%B8%B2%E0%B8%A3%E0%B8%B2%E0%B8%87%E0%B8%AA%E0%B8%B5%E0%B8%9C%E0%B9%89%E0%B8%B2_%E0%B8%9E%E0%B8%B2%E0%B8%AA_6/%E0%B9%84%E0%B8%8B%E0%B8%AA%E0%B9%8C%E0%B9%80%E0%B8%AA%E0%B8%B7%E0%B9%89%E0%B8%AD_%E0%B9%80%E0%B8%AA%E0%B8%B7%E0%B8%AD%E0%B8%A2%E0%B8%B7%E0%B8%94%E0%B8%AB%E0%B8%8D%E0%B8%B4%E0%B8%87.jpg', 'ผู้หญิง')}>ผู้หญิง
       </Button>
